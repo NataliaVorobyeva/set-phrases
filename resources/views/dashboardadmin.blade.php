f220290cf33933a41d61967fe68342c935abb260
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-02-16 15:54:52 +0100

    mas cambios a dashboard admin

--- resources/views/dashboardadmin.blade.php
+++ resources/views/dashboardadmin.blade.php
@@ -8,13 +8,65 @@
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Frases</a>
       <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Agregar</a></li>
-        <li><a class="dropdown-item" href="#">Editar</a></li>
+        <li><a class="dropdown-item" href="#">Agregar nueva frase</a></li>
         <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Agregar al carrusel</a></li>
+        <li><a class="dropdown-item" href="#">Agregar frase al carrusel</a></li>
       </ul>
     </li>
   </ul>
-  <h2>Contenedor de frases</h2>
+  <h4>Contenedor de frases</h2>
+    <div class="container-md d-flex justify-content-around">
+
+        <div class="card text-white" style="width: 18rem;">
+            <img class="card-img" src="{{URL::asset('/imgs/fondo-exemple.webp')}}" alt="Card image">
+            <div class="card-img-overlay">
+            <h5 class="card-title">Some representative placeholder content for the first slide of the carousel.</h5>
+            <p class="card-text">Author</p>
+            </div>
+            <div class="card-body d-flex justify-content-around">
+                <a href="#" class="btn btn-primary">Borrar</a>
+                <a href="#" class="btn btn-primary">Editar</a>
+            </div>
+        </div>
+
+        <div class="card text-white" style="width: 18rem;">
+            <img class="card-img" src="{{URL::asset('/imgs/fondo-exemple.webp')}}" alt="Card image">
+            <div class="card-img-overlay">
+            <h5 class="card-title">Some representative placeholder content for the first slide of the carousel.</h5>
+            <p class="card-text">Author</p>
+            </div>
+            <div class="card-body d-flex justify-content-around">
+                <a href="#" class="btn btn-primary">Borrar</a>
+                <a href="#" class="btn btn-primary">Editar</a>
+            </div>
+        </div>
+
+        <div class="card text-white" style="width: 18rem;">
+            <img class="card-img" src="{{URL::asset('/imgs/fondo-exemple.webp')}}" alt="Card image">
+            <div class="card-img-overlay">
+            <h5 class="card-title">Some representative placeholder content for the first slide of the carousel.</h5>
+            <p class="card-text">Author</p>
+            </div>
+            <div class="card-body d-flex justify-content-around">
+                <a href="#" class="btn btn-primary">Borrar</a>
+                <a href="#" class="btn btn-primary">Editar</a>
+            </div>
+        </div>
+
+        <div class="card text-white" style="width: 18rem;">
+            <img class="card-img" src="{{URL::asset('/imgs/fondo-exemple.webp')}}" alt="Card image">
+            <div class="card-img-overlay">
+            <h5 class="card-title">Some representative placeholder content for the first slide of the carousel.</h5>
+            <p class="card-text">Author</p>
+            </div>
+            <div class="card-body d-flex justify-content-around">
+                <a href="#" class="btn btn-primary">Borrar</a>
+                <a href="#" class="btn btn-primary">Editar</a>
+            </div>
+        </div>
+
+    </div>
+</main>
+
 
 @include('footer')
