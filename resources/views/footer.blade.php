f3e1cf84be732af83c9f73045fc39affc3e7296b
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-02-16 13:51:38 +0100

    carrusel, footer y escilos todos

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-    <footer>
-      <footer class="footer mt-auto py-3 bg-danger bg-gradient">
-        <div class="container d-flex flex-column ">
-          <span class="text-muted">Tus frases inspiradoras Coorp.</span>
-          <span class="text-muted"><small>Calle de los Remedios 111. España.</span></span>
-        </div>
-      </footer>
-    </footer>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-=======
   <footer class="container">
     <p class="float-end"><a href="#">Back to top</a></p>
     <p>&copy; 2022 Inspirate, Inc. </p>
@@ -16,6 +5,5 @@
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
->>>>>>> b935a3ab9cdc4795c1a6ce196a7c76d42b7ef49b
 </body>
 </html>
