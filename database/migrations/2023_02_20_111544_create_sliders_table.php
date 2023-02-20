b6f80096d3ccc008898ed7c1aeabcaf3476f3857
Crisktina <crishidalgolopez@gmail.com>
2023-02-20 12:27:27 +0100

    creado model mcr

--- database/migrations/2023_02_20_111544_create_sliders_table.php
+++ database/migrations/2023_02_20_111544_create_sliders_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
+            $table->integer('frase_id');
             $table->timestamps();
         });
     }
