f4826f0f43a01aa4f615bcd2769155175d6bd036
Crisktina <crishidalgolopez@gmail.com>
2023-02-28 01:13:01 +0100

    no imprime bien en likes

--- database/migrations/2023_02_14_175752_create_likes_table.php
+++ database/migrations/2023_02_14_175752_create_likes_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->integer('user_id');
             $table->integer('frase_id');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
